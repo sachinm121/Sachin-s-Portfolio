e22cdebaba981d8e7075d94d70513467796112c4
= <sachinmahor685@gmail.com>
2024-04-30 16:02:51 +0530

    Project added

--- src/components/Projects/ProjectsFile.js
+++ src/components/Projects/ProjectsFile.js
@@ -1,24 +1,34 @@
 import expense from '../../assets/projects/expense.png';
 import HMS from '../../assets/projects/HMS.png';
+import CV from "../../assets/projects/CV.png"
 export const projectsFile1 = [
     {
-        name: "Book My Meal Online",
-        desc: "It is an online food booking plateform. Here, a user of Bhopal can login and can search a restaurent of his choice, can order food of his desire.",
-        github: "",
-        image: expense,
-        demo: "https://simmisheetaljain.github.io/html-bookmymeal.com/",
-        tech: ["HTML", "CSS"],
+        name: "NIT JSR Complaint App(CampusVoice)",
+        desc: "This is MERN based app focusing on student complaint for NIT Jamshedpur, Here student can sign and login and can admit thier complaints.",
+        github: "https://github.com/sachinm121/CampusVoice",
+        image: CV,
+        demo: "https://github.com/sachinm121/CampusVoice",
+        tech: ["React", "Node", "MongoDB", "JWT", "AntDesign", "Tailwind", "Nodemailer", "Cloudinary"],
         flag: false
+    },
+    {
+        name: "Expenses Management App",
+        desc: "It is an Expense Management App. User can sign and login and then user can manage thier expenses",
+        github: "https://github.com/sachinm121/expense-management-system",
+        image: expense,
+        demo: "https://github.com/sachinm121/expense-management-system",
+        tech: ["React", "Node", "Expressjs", "MongoDB", "AntDesign"],
+        flag: true
     },
     {
         name: "Hospital Management System",
         desc: "The software developed is Hospital Management System Software that is designed by Sheetal Jain and Sachin Mahor.It has been designed keeping in mind today’s complex healthcare system’s requirements and the importance of TIME.",
-        github: "",
+        github: "https://github.com/sachinm121/Hospital-Managment-System",
         image: HMS,
-        demo: "https://simmisheetaljain.github.io/html-bookmymeal.com/",
-        tech: ["Core Java", "Swing", "AWT"],
-        flag: true
-    }
+        demo: "https://github.com/sachinm121/Hospital-Managment-System",
+        tech: ["CoreJava", "Swing", "AWT"],
+        flag: false
+    },  
 ]
 
 // name, desc, github, image, demo, tech, flag 
