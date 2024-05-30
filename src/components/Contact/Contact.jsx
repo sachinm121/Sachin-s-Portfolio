8df176ab296a7efdaebb17c2ed4fbe767f67444d
= <sachinmahor685@gmail.com>
2024-05-30 11:13:20 +0530

    add more links

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -32,7 +32,7 @@ const Contact = () => {
             <p className="text-[24px] font-bold border-b-4 border-blue-500 pb-3">
               Find Sachin elsewhere
             </p>
-            <div className="grid grid-cols-2 mt-5 gap-4 text-[30px]">
+            <div className="flex mt-5 gap-10 text-[30px]">
               <a
                 href="https://www.linkedin.com/in/sachin-mahor-769a33222/"
                 target="_blank"
@@ -42,13 +42,22 @@ const Contact = () => {
               <a href="https://github.com/sachinm121" target="_blank">
                 <IoLogoGithub className="hover:text-[#03fcc6]" />
               </a>
-              <FaWhatsapp className="hover:text-[#03fcc6]" />
-              <FaTelegramPlane className="hover:text-[#03fcc6]" />
+              <a href="https://wa.me/qr/C2KEKS2N6PM4K1">
+                <FaWhatsapp className="hover:text-[#03fcc6]" />
+              </a>
+              <a href="https://t.me/+918791457401">
+                <FaTelegramPlane className="hover:text-[#03fcc6]" />
+              </a>
             </div>
           </div>
         </div>
-        <a className="mt-10 object-fill w-fit" href="mailto:sachinmahor685@gmail.com">
-          <button className="bg-yellow-600 px-5 py-3 rounded-md text-[20px] font-bold hover:bg-yellow-500">Write Sachin</button>
+        <a
+          className="mt-10 object-fill w-fit"
+          href="mailto:sachinmahor685@gmail.com"
+        >
+          <button className="bg-yellow-600 px-5 py-3 rounded-md text-[20px] font-bold hover:bg-yellow-500">
+            Write Sachin
+          </button>
         </a>
       </div>
     </div>
