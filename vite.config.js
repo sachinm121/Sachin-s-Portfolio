dc6f0d77521a0dd14f71bb009409d155859688c7
Sachinm121 <sachinmahor685@gmail.com>
2024-08-01 11:11:07 +0530

    updated

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/Sachin-s-Portfolio/",
   plugins: [react()],
+  build: {
+    sourcemap: true, // Enable CSS source maps
+  },
 })
